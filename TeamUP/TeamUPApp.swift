df0f56d195f289894ca940415dc53a2d82373cb9
Marc Fernández <marcausente@gmail.com>
2025-02-07 15:35:27 +0100

    Initial Commit

---
+++ TeamUP/TeamUPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamUPApp.swift
+//  TeamUP
+//
+//  Created by Marc Fernández on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TeamUPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
