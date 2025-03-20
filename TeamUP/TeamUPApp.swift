b9988d8fe879ab4d6422c8e6a76cbd7065f37f9e
Marc Fernández <marcausente@gmail.com>
2025-03-20 17:03:43 +0100

    FEAT: Push test on new branch

--- TeamUP/TeamUPApp.swift
+++ TeamUP/TeamUPApp.swift
@@ -12,7 +12,7 @@ struct TeamUPApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView() //TEST
                 .environmentObject(authManager)
         }
     }
